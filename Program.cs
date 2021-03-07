961ea89aa34588685034ce4784a87f3fb676f752
camila <camilasangobar@gmail.com>
2021-03-07 00:53:40 -0300

    contagem

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace contagmeregressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            
+            Console.WriteLine("preparar...");
+            Console.ReadKey();
+            Console.WriteLine("apontar...");
+            Console.ReadKey();
+            Console.WriteLine("FOGO!!!");
+            Console.WriteLine();
+        }
+    }
+}
